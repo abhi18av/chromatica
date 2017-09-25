5622c9d714550f16ab4e0d017d3075d9e35cf988
Abhinav Sharma <abhi18av@gmail.com>
2017-09-26 00:08:23 +0530

    added the dev folder from chestnut

---
+++ dev/cljs/user.cljs
@@ -0,0 +1,8 @@
+(ns cljs.user
+  (:require [chromatica.core]
+            [chromatica.system :as system]))
+
+(def go system/go)
+(def reset system/reset)
+(def stop system/stop)
+(def start system/start)
