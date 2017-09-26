a6e529e59c5ba9d07a2495c7fe714e2d9659c0fe
Abhinav Sharma <abhi18av@gmail.com>
2017-09-26 19:42:01 +0530

    routine commit

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-beta1"]
+  :dependencies [
+
+                 [org.clojure/clojure "1.9.0-beta1"]
                  [org.clojure/clojurescript "1.9.908" :scope "provided"]
                  [com.cognitect/transit-clj "0.8.300"]
                  [org.clojure/tools.namespace "0.2.11"]
