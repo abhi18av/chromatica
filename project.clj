f695293ddea3e2f51d8611aa7066a0aae9f6ff79
Abhinav Sharma <abhi18av@gmail.com>
2017-09-26 19:20:03 +0530

    formatted the project.clj

--- project.clj
+++ project.clj
@@ -3,65 +3,51 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [
-
-                [org.clojure/clojure "1.9.0-beta1"]
+  :dependencies [[org.clojure/clojure "1.9.0-beta1"]
                  [org.clojure/clojurescript "1.9.908" :scope "provided"]
                  [com.cognitect/transit-clj "0.8.300"]
                  [org.clojure/tools.namespace "0.2.11"]
 
-[environ "1.1.0"]
+                 [environ "1.1.0"]
 
-                             [com.cemerick/piggieback "0.2.2"]
-                             [org.clojure/tools.nrepl "0.2.13"]
-                             [lein-doo "0.1.7"]
-                             [reloaded.repl "0.2.3"]
-                 ]
-
-
-     :plugins [[lein-cljsbuild "1.1.6"]]
-
+                 [com.cemerick/piggieback "0.2.2"]
+                 [org.clojure/tools.nrepl "0.2.13"]
+                 [lein-doo "0.1.7"]
+                 [reloaded.repl "0.2.3"]] :plugins [[lein-cljsbuild "1.1.6"]]
 
 ;              :plugins [ [lein-doo "0.1.7"]]
+  :min-lein-version "2.6.1" :source-paths ["src/clj" "src/cljs" "src/cljc"]
 
-
-
-     :min-lein-version "2.6.1"
-
-
-     :source-paths ["src/clj" "src/cljs" "src/cljc"]
-
-     :test-paths ["test/clj" "test/cljc"]
+  :test-paths ["test/clj" "test/cljc"]
 
   ;;   :clean-targets ^{:protect false} [:target-path :compile-path "resources/public/js"]
 
-     :uberjar-name "chromatica.jar"
+  :uberjar-name "chromatica.jar"
 
   ;;   ;; nREPL by default starts in the :main namespace, we want to start in `user`
   ;;   ;; because that's where our development helper functions like (go) and
   ;;   ;; (browser-repl) live.
 ;    :repl-options {:init-ns user}
 
-      :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
+  :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
 
-    :cljsbuild {:builds
-                [{:id "puppeteer"
-                  :source-paths ["src/cljc" "src/cljs"]
-                  :compiler {:main chromatica.core
-                             :target :nodejs
+  :cljsbuild {:builds
+              [{:id "puppeteer"
+                :source-paths ["src/cljc" "src/cljs"]
+                :compiler {:main chromatica.core
+                           :target :nodejs
                              ;:asset-path "js/compiled/out"
-                             :output-to "resources/public/js/compiled/chromatica.js"
-                             :output-dir "resources/public/js/compiled/out"
-                             :pretty-print true
-                             :optimizations :none
+                           :output-to "resources/public/js/compiled/chromatica.js"
+                           :output-dir "resources/public/js/compiled/out"
+                           :pretty-print true
+                           :optimizations :none
                              ;:optimizations :advanced :simple :whitespace :none
-                             :parallel-build true
-                             :npm-deps {"chalk" "2.1.0"}
-                             :install-deps true
-                             :source-map-timestamp true
+                           :parallel-build true
+                           :npm-deps {"chalk" "2.1.0"}
+                           :install-deps true
+                           :source-map-timestamp true
                              ;:source-map true
-                             :warnings false}}]}
-
+                           :warnings false}}]}
 
 ;  :main ^:skip-aot chromatica.core
   :target-path "target/%s"
